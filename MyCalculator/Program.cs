1f20106aec66369106231984ea1c9e0cc6c94abf
synset <synnove.settman@yahoo.com>
2020-05-15 15:49:23 +0200

    Add project files.

---
+++ MyCalculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first number: ");
+            string inputFirstNr = (Console.ReadLine());
+            int firstNr;
+            Int32.TryParse(inputFirstNr, out firstNr);
+
+            Console.WriteLine("Enter operation of choice ( +, -, / or *)");
+            string userOperator = Console.ReadLine();
+
+            Console.WriteLine("Please enter your second number: ");
+            string inputSecondNr = (Console.ReadLine());
+            int secondNr;
+            Int32.TryParse(inputSecondNr, out secondNr);
+
+           
+            switch (userOperator)
+            {
+                
+                case "-":
+                    int result = firstNr - secondNr;
+                    Console.WriteLine(firstNr + " - " + secondNr + " = " + result);
+                    break;
+
+                case "+":
+                    result = firstNr + secondNr;
+                    Console.WriteLine(firstNr + " + " + secondNr + " = " + result);
+                    break;
+
+                case "/":
+                    result = firstNr / secondNr;
+                    Console.WriteLine(firstNr + " / " + secondNr + " = " + result);
+                    break;
+
+                case "*":
+                    result = firstNr * secondNr;
+                    Console.WriteLine(firstNr + " * " + secondNr + " = " + result);
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose one of the operators: -, +, / or *");
+                    break;
+            }
+        }
+    }
+}
